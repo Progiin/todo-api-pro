8deeda60cc710134a71869c0301b7385372ce6c1
Progiin <142786274+Progiin@users.noreply.github.com>
2026-04-14 18:42:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 let tasks = [];
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.get("/tasks", (req, res) => res.json(tasks));
 
 app.post("/tasks", (req, res) => {
@@ -17,4 +21,15 @@ app.post("/tasks", (req, res) => {
   res.json(task);
 });
 
-app.listen(3000, () => console.log("rodando 🚀"));
+app.put("/tasks/:id", (req, res) => {
+  const task = tasks.find(t => t.id == req.params.id);
+  if (task) task.done = true;
+  res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  tasks = tasks.filter(t => t.id != req.params.id);
+  res.send("deleted");
+});
+
+app.listen(3000, () => console.log("🔥 rodando"));
